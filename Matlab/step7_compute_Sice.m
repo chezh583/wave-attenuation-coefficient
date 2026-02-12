d436a46e9ddd41b43ea61c7d8ff2c1c14de3e5b3
Zhaohui Cheng <160224408+chezh583@users.noreply.github.com>
2026-02-12 11:05:52 +0100

    Add files via upload

--- Matlab/step7_compute_Sice.m
+++ Matlab/step7_compute_Sice.m
@@ -1,7 +1,6 @@
 clear;clc;
 close all
 
-%purpose: estimate attenuation coefficients. 
 
 %% dispersion relation, mass-loading model
 frq=zeros(25,1);
@@ -21,39 +20,47 @@ d=500;
 source_dir='expoutput/';
 %=========================
 
-%%
+
 list=importdata('output/list_rotated.mat');
 dic=importdata("interpolated_variables/dictionary.txt");
 len=length(dic);
 out_ki=zeros(25,len);
-
-
-
+out_ki_simple=zeros(25,len);
+cut_num=zeros(1,len);
+load('interperation_results/buoylist1.mat'); %dim: buoy_frq,num
+load('interperation_results/buoylist2.mat');
+load('output/Ef_step6.mat');
+%========================
+method=2;
+%========================
 %%
-
-
-
-
-i=11;
+i=187;
+load('output/out_ki.mat');
+load('output/out_ki_simple.mat');
 temparray=importdata(['interpolated_variables/',num2str(dic(i)),'/sic.txt']);
 cut=length(temparray);
 Ef=zeros(25,cut);
-Sin=Ef; Sds=Ef; Snl=Ef;
+Si=Ef; Sds=Ef; Snl=Ef;
+A=log10(Ef_step6(:,1,i));
+B=log10(Ef_step6(:,2,i));
 for j=1:cut
     fname=[source_dir,num2str(dic(i)),'_cut',num2str(j),'.nc'];
-    ef=ncread(fname,'ef'); ef=squeeze(ef(:,end,end)); Ef(:,j)=ef;
+    alpha= (j-1) / (cut-1);
+    interp_wvsp=(1 - alpha) * A + alpha * B;
+    interp_wvsp=10.^interp_wvsp;
+    Ef(:,j)=interp_wvsp;
     sin=ncread(fname,'sin'); sin=squeeze(sin(:,end,end)); Sin(:,j)=sin;
     sds=ncread(fname,'sds'); sds=squeeze(sds(:,end,end)); Sds(:,j)=sds;
     snl=ncread(fname,'snl'); snl=squeeze(snl(:,end,end)); Snl(:,j)=snl;
     f=ncread(fname,'frequency');
 end
-figure, subplot(121)
-plot(f,Ef,'k'), xlim([0.05 0.25])
-subplot(122)
-plot(f,Sin,'k--'), hold on
-plot(f,Sds,':');
-plot(f,Snl), xlim([0.05 0.25])
-title(source_dir)
+% figure, subplot(121)
+% plot(f,Ef,'k'), xlim([0.05 0.25])
+% subplot(122)
+% plot(f,Sin,'k--'), hold on
+% plot(f,Sds,':');
+% plot(f,Snl), xlim([0.05 0.25])
+% title(source_dir)
 
 
 
@@ -63,8 +70,8 @@ subplot(131)
 loglog(f,Ef(:,1),'k','LineWidth',1)
 hold on
 loglog(f,Ef(:,end),'k--','LineWidth',1)
-y=1e-9*f(10:23).^(-4);
-loglog(f(10:23),y,'LineWidth',1)
+% y=1e-9*f(10:23).^(-4);
+% loglog(f(10:23),y,'LineWidth',1)
 xlabel('$f$ (Hz)','fontsize',16,'FontName','times','Interpreter','latex')
 ylabel('PSD (m$^2$/Hz)','fontsize',16,'FontName','times','Interpreter','latex')
 xlim([0.05 0.25])
@@ -75,17 +82,17 @@ yyaxis left
 plot(f,Sin(:,1),'k','LineWidth',1)
 hold on
 plot(f,Sin(:,end),'k--','LineWidth',1)
-ylabel('$S_{in}$ (m$^{-1}$)','fontsize',16,'FontName','times','Interpreter','latex')
-
+ylabel('$S_{in}$ (m$^2$/Hz/s)','fontsize',16,'FontName','times','Interpreter','latex')
+ylim([-2e-7 2e-7])
 
 yyaxis right
 plot(f,Snl(:,1),'LineWidth',1,'Color',[0.6 0.6 0.6])
 hold on
 plot(f,Snl(:,end),'--','LineWidth',1,'Color',[0.6 0.6 0.6])
 xlim([0.05 0.25])
-ylabel('$S_{nl}$ (m$^{-1}$)','fontsize',16,'FontName','times','Interpreter','latex')
+ylabel('$S_{nl}$ (m$^2$/Hz/s)','fontsize',16,'FontName','times','Interpreter','latex')
 legend('$S_{in}$ upstream','$S_{in}$ downstream','$S_{nl}$ upstream','$S_{nl}$ downstream','Interpreter','latex','fontsize',14,'fontname','times')
-ylim([-7e-11 5e-11])
+ylim([-3e-14 3e-14])
 
 ax = gca;
 ax.YAxis(1).Color = 'k';
@@ -104,11 +111,6 @@ legend('$k_i$ simple','$k_i$ full','Interpreter','latex','fontsize',14,'fontname
 
 
 
-
-
-
-
-method=2;
 %%
 if (method==1)
 % method1: calculate Sice in each cut, and average them to get final results
@@ -197,7 +199,6 @@ end
 
 %%
 if(method==2)
-if(method==2)
 % method 2: calculate non-Sic in each cut, and get Sic from residual
  disparity=ones(len,1);
 % % disparity([ 4 5 6 8 9 10 11 13 14 15 19 25 26 33 34 40 41 42 43 44 45 46 47 48 52 53 58 61 62 63 64 65 66 67 71 72])=0;
@@ -212,10 +213,21 @@ if(method==2)
          Sin=zeros(25,cut);
          Sds=zeros(25,cut);
          Snl=zeros(25,cut);
-         Sice=zeros(25,len);         
+         Sice=zeros(25,len);
+         A=log10(Ef_step6(:,1,i));
+         B=log10(Ef_step6(:,2,i));
+
          for j=1:cut
              fname=[source_dir,num2str(dic(i)),'_cut',num2str(j),'.nc'];
-             ef=ncread(fname,'ef'); ef=squeeze(ef(:,end,end)); Ef(:,j)=ef;
+             % efth=ncread(['interpolated_variables/',num2str(dic(i)),'/cut',num2str(j),'.nc'],'efth');
+             % efth=squeeze(efth(:,:,end,end));
+             % efth=sum(efth);
+             % Ef(:,j)=efth';
+             alpha= (j-1) / (cut-1);
+             interp_wvsp=(1 - alpha) * A + alpha * B;
+             interp_wvsp=10.^interp_wvsp;
+             Ef(:,j)=interp_wvsp;
+             %ef=ncread(fname,'ef'); ef=squeeze(ef(:,end,end)); Ef(:,j)=ef;
              sin=ncread(fname,'sin'); sin=squeeze(sin(:,end,end)); Sin(:,j)=sin;
              sds=ncread(fname,'sds'); sds=squeeze(sds(:,end,end)); Sds(:,j)=sds;
              snl=ncread(fname,'snl'); snl=squeeze(snl(:,end,end)); Snl(:,j)=snl;
@@ -231,25 +243,23 @@ if(method==2)
          term3_int=zeros(25,1);
          for dx=2:cut
              c=0.5*(ICE(dx-1)+ICE(dx));%
-             % c=0.44*c;
              d=D(dx)-D(dx-1);
  
              %dispersion relation
-             %h(i)=1; %for testing
+             % h(i)=1; %for testing
              % kr=omega.^2./(g-c*rho_ice*h(i)*omega.^2/rho_sea);
              % cg=diff(omega)./diff(kr); cg=[cg;cg(end)+cg(end)-cg(end-1)];
 
              k=omega.^2/g;
              cp=sqrt(g*tanh(k*d)./k);
              cg=0.5*(1+(2*k*d)./(sinh(2*k*d))).*cp;
- 
+             % 
              % term0=term0+cg.*(Ef(:,dx)-Ef(:,dx-1))./d;
              % term1=term1+(1-c)*0.5*(Sin(:,dx-1)+Sin(:,dx)+Sds(:,dx-1)+Sds(:,dx));
              % term2=term2+0.5*(Snl(:,dx-1)+Snl(:,dx));
              % term3_int=term3_int-2*c*cg.*0.5.*(Ef(:,dx-1)+Ef(:,dx));
 
              term0=term0+cg.*Ef(:,dx)-cg.*Ef(:,dx-1);
-             % term0=term0-(log(Ef(:,dx)./Ef(:,dx-1)))/d;
              term1=term1+0.5*((Sin(:,dx-1)+Sds(:,dx-1))*(1-c)+(Sin(:,dx)+Sds(:,dx))*(1-c))*d;
              term2=term2+0.5*(Snl(:,dx-1)+Snl(:,dx))*d;
              term3_int=term3_int+0.5*(-2*c*cg.*Ef(:,dx-1)-2*c*cg.*Ef(:,dx))*d;
@@ -257,6 +267,7 @@ if(method==2)
          term3=term0-term1-term2;
          out_ki(:,i)=term3./term3_int;
          out_ki_simple(:,i)=term0./term3_int;
+         cut_num(1,i)=cut;
      end
  
      % va=var(ki');
@@ -272,16 +283,25 @@ if(method==2)
 
 end
 
-end
-
+figure
+plot(frq, term0)
+hold on
+plot(frq, term3)
+figure
+plot(frq, term1)
+figure
+plot(frq, term2)
+figure
+loglog(frq,out_ki(:,i))
 
 %% apparent attenuation
 
-buoyspec1=importdata(['output/buoylist1.mat']);  % wave spectrum collected by buoy1
-buoyspec2=importdata(['output/buoylist2.mat']);  % wave spectrum collected by buoy2
+buoyspec1=importdata(['interperation_results/buoylist1.mat']);  % wave spectrum collected by buoy1
+buoyspec2=importdata(['interperation_results/buoylist2.mat']);  % wave spectrum collected by buoy2
 position=importdata(['output/position.mat']);
 
 alpha=zeros(25,len);
+onealpha=zeros(len,1);
 D=position(:,3);
 dic=num2str(dic);
 for i=1:len
@@ -295,60 +315,28 @@ for i=1:len
         b2=buoyspec1(:,inum);b1=buoyspec2(:,inum);
     end
     alpha(:,i)=log(b1./b2)/(D(inum));
+
+    onealpha(i)=log(trapz(frq,b1)./trapz(frq,b2))/(2*D(inum));
     end
 end
 
-ALP=zeros(len,1);
-for i=1:len
-    if(disparity(i))
-    templabel=str2num(dic(i,:));
-    inum=find(list(:,end)==templabel);
-    if(position(inum,1)==1)
-        b1=trapz(f,buoyspec1(:,inum));b2=trapz(f,buoyspec2(:,inum));
-    end
-    if(position(inum,1)==2)
-        b2=trapz(f,buoyspec1(:,inum));b1=trapz(f,buoyspec2(:,inum));
-    end
-    ALP(i)=log(b1./b2)/(D(inum));
-    end
-end
 
-figure
-plot(f,buoyspec1(:,inum));
-hold on
-plot(f,buoyspec2(:,inum));
-
-figure
-plot(f,alpha(:,1));
-hold on
-plot(f,ALP(1)*ones(25,1));
-plot(f,trapz(f,alpha(:,1))*ones(25,1))
-plot(f,mean(alpha(:,1))*ones(25,1))
-
-legend('alpha(f)','alpha_sig','int(f,alpha(f))','mean(alpha(f))')
 %%
 
 figure
-subplot(131)
+subplot(121)
 loglog(frq,alpha,'Color',[0.3 0.3 0.3])
 xlim([0.05 0.25])
 xlabel('Frequency (Hz)')
 ylabel('Apparent attenuation rate (m^{-1})')
 ylim([1e-8 1e-1])
-subplot(132)
+subplot(122)
 loglog(frq,out_ki,'Color',[0.3 0.3 0.3])
 xlim([0.05 0.25])
 xlabel('Frequency (Hz)')
 ylabel('Amplitude attenuation rate (m^{-1})')
 ylim([1e-7 1e0])
-subplot(133)
-loglog(frq,out_ki_simple,'Color',[0.3 0.3 0.3])
-
-save('output/alpha.mat',"alpha");
-save('output/out_ki.mat',"out_ki");
-save('output/out_ki_simple.mat',"out_ki_simple");
-
-
+sgtitle(source_dir)
 % a=0.05:0.01:0.25;
 % b1=a.^1.9;
 % b2=a.^2.9;
@@ -359,6 +347,9 @@ save('output/out_ki_simple.mat',"out_ki_simple");
 % plot(a,b3,'LineWidth',2)
 
 
+save('output/alpha.mat',"alpha");
+save('output/out_ki.mat',"out_ki");
+save('output/out_ki_simple.mat',"out_ki_simple");
 % dlmwrite('out_ki2.txt', out_ki, '-append', 'delimiter', ' ','precision','%.10f');
 % dlmwrite('alpha2.txt', alpha, '-append', 'delimiter', ' ','precision','%.10f');
  % saveas(gcf,'fig2.png')
